67ac9bfb772b792d4f598bbf24246966febea47b
Naomi Justin <naomi.z.justin@gmail.com>
2022-09-01 20:59:44 +1000

    Toast Disclaimer JS

--- js/script.js
+++ js/script.js
@@ -18,6 +18,14 @@ function initPageContent() {
     function showHowToToast () {
         $('#toastHowToPrompt').toast('show');
     }
+
+    // ========= Toast Disclaimer =========
+    function showToast () {
+        $('#toastTipPrompt').toast('show');
+    }
+    $('#toastInfoIcon').click(function(){
+        showToast();
+    });
 }
 $(document).ready(function(){
     initPageContent();    
