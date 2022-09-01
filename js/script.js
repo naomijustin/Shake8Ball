6298a1c26fad7d8b30a47b272a0817e35e71d915
Naomi Justin <naomi.z.justin@gmail.com>
2022-09-01 20:58:18 +1000

    Added Toast help JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
+// ===== Created by Naomi Justin =====
 "use strict"
+
+function initPageContent() {
+    // === Global Variables ===    
+    var firstShakeCompleted = false;    
+    showToast();
+    setTimeout(function(){
+        if (!firstShakeCompleted) {
+            showHowToToast();
+        }        
+    }, 6500);
+
+    // === Toast Help ===
+    $('#toastHelpIcon').click(function(){
+        showHowToToast();
+    });
+    function showHowToToast () {
+        $('#toastHowToPrompt').toast('show');
+    }
+}
+$(document).ready(function(){
+    initPageContent();    
+});
+// ===== Created by Naomi Justin =====
